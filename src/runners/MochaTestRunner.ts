c62311b91b602f5e8f4028f52001b6e139f389ef
liuweiqing <weak_ptr@outlook.com>
2021-03-18 14:36:16 +0800

    fix: escape special character for mocha --grep

--- src/runners/MochaTestRunner.ts
+++ src/runners/MochaTestRunner.ts
@@ -20,51 +20,36 @@ export class MochaTestRunner implements ITestRunnerInterface {
     this.configurationProvider = configurationProvider;
   }
 
-  public runTest(
-    rootPath: WorkspaceFolder,
-    fileName: string,
-    testName: string
-  ) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+  private escapeRegExp(testName: string): string {
+    return testName.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+  }
+
+  public runTest(rootPath: WorkspaceFolder, fileName: string, testName: string) {
     const additionalArguments = this.configurationProvider.additionalArguments;
-    const environmentVariables = this.configurationProvider
-      .environmentVariables;
+    const environmentVariables = this.configurationProvider.environmentVariables;
 
-    const command = `${
-      this.binPath
-    } ${fileName} --grep="${testName}" ${additionalArguments}`;
+    testName = this.escapeRegExp(testName);
 
-    const terminal = this.terminalProvider.get(
-      { env: environmentVariables },
-      rootPath
-    );
+    const command = `${this.binPath} ${fileName} --grep="${testName}" ${additionalArguments}`;
+    const terminal = this.terminalProvider.get({ env: environmentVariables }, rootPath);
 
     terminal.sendText(command, true);
     terminal.show(true);
   }
 
-  public debugTest(
-    rootPath: WorkspaceFolder,
-    fileName: string,
-    testName: string
-  ) {
+  public debugTest(rootPath: WorkspaceFolder, fileName: string, testName: string) {
     const additionalArguments = this.configurationProvider.additionalArguments;
-    const environmentVariables = this.configurationProvider
-      .environmentVariables;
+    const environmentVariables = this.configurationProvider.environmentVariables;
 
     debug.startDebugging(rootPath, {
-      args: [
-        fileName,
-        "--grep",
-        testName,
-        "--no-timeout",
-        ...additionalArguments.split(" ")
-      ],
+      args: [fileName, "--grep", testName, "--no-timeout", ...additionalArguments.split(" ")],
       console: "integratedTerminal",
       env: environmentVariables,
       name: "Debug Test",
       program: "${workspaceFolder}/node_modules/mocha/bin/_mocha",
       request: "launch",
-      type: "node"
+      type: "node",
     });
   }
 }
